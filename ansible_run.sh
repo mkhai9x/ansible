4853880e687c2e6020c6e3ad18e6f64d4c4b4c23
Minh Tran <mkhai9x@gmail.com>
2024-07-07 14:33:00 +1000

    clean up

--- ansible_run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-## Install ansible
-sudo apt-add-repository -y ppa:ansible/ansible
-sudo apt-get update -y
-sudo apt-get install -y curl git software-properties-common ansible
-
-## pull ansible
-sudo ansible-pull -U https://github.com/mkhai9x/ansible.git
